99e92480809091bbb4c822c249da27e291b1321c
Danilo <danilovera02@gmail.com>
2022-06-09 17:53:22 -0500

    solucionando

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -9,28 +9,27 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 
 export default class AppRoutes extends Component {
-    
   render() {
+    //hola
     const GlobalStyled = createGlobalStyle`
     body{
       margin: 0px;
       padding: 0px;
       box-sizing: border-box;
     }
-    `
+    `;
     return (
       <div>
-
-      <GlobalStyled/>
+        <GlobalStyled />
         <Router>
           <Routes>
             <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
             <Route exact path="/search" element={<Search />} />
-            <Route exact path="/search" element={<Profile />} />
+            <Route exact path="/profile" element={<Profile />} />
             <Route exact path="/" element={<Homepage />} />
           </Routes>
-          <Footer/>
+          <Footer />
         </Router>
       </div>
     );
