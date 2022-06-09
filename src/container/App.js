7ba347c3e22982c6d039482278f98b85ef134873
Danilo <danilovera02@gmail.com>
2022-06-09 15:04:27 -0500

    agg Global style

--- src/container/App.js
+++ src/container/App.js
@@ -1,7 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
 
 function App() {
+
+  const GlobalStyled = createGlobalStyle`
+  body{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+  `
   return (
     <div className="App">
+    <GlobalStyled />
     
     </div>
   );
